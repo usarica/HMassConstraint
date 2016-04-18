2e2eccf9787d2ce336101b2857b460ae1b570b1e
usarica <ulascan90@gmail.com>
2016-04-18 23:03:23 +0200

    Massive improvements

--- include/HMassConstraint.h
+++ include/HMassConstraint.h
@@ -7,8 +7,8 @@
 #include <fstream>
 #include <cmath>
 #include <map>
-#include "TString.h"
 #include "TLorentzVector.h"
+#include "TString.h"
 #include "TMatrixDSym.h"
 #include "RooRealVar.h"
 #include "RooArgSet.h"
@@ -26,33 +26,85 @@
 #include <DataFormats/PatCandidates/interface/PFParticle.h>
 
 
-using namespace std;
-using namespace reco;
-using namespace pat;
-
 class HMassConstraint {
 public:
 
-   // Member functions
-   HMassConstraint(RooSpin::VdecayType Vdecay1_, RooSpin::VdecayType Vdecay2_); // See RooSpin.h for what enumerators are available.
-   void reset(); // { if(fit_res!=0) delete fit_res; }
-   void addLeptons(const vector<map<Candidate*, PFParticle*>>& FermionFSR); // To set the Lepton and photon arrays in a map form. Pass null-pointer if the photon does not exist.
-   RooFitResult* fit(); // Does the fit and returns a pointer to the fitresult.
+  // Member functions
+  HMassConstraint(Double_t sqrts_, RooSpin::VdecayType Vdecay1_, RooSpin::VdecayType Vdecay2_); // See RooSpin.h for what enumerators are available.
+  virtual ~HMassConstraint();
 
-   // Data members
-   RooSpin* pdf;
-   SpinPdfFactory* pdfFactory;
+  void reset(); // { if(fit_res!=0) delete fit_res; }
+
+  void addFermionsWithFSR(const std::vector<map<reco::Candidate*, pat::PFParticle*>>& FermionWithFSR); // To set the Lepton and photon arrays in a map form. Pass null-pointer if the photon does not exist.
+
+  RooFitResult* fit(); // Does the fit and returns a pointer to the fitresult.
+
+  // Data members
+  RooSpin* pdf;
+  SpinPdfFactory* pdfFactory;
 
 private:
 
-   RooSpin::modelMeasurables measurables;
-   RooFitResult* fitRes;
+  RooRealVar* mass_ferm[2][2];
+  RooRealVar* pT_ferm[2][2];
+  RooRealVar* eta_ferm[2][2];
+  RooRealVar* phi_ferm[2][2];
+  RooRealVar* pTbar_ferm[2][2];
+  RooRealVar* etabar_ferm[2][2];
+  RooRealVar* phibar_ferm[2][2];
 
-   vector<map<Candidate*,TMatrixDSym>> fermCovMatrix; // Maximum 4 x (3x3)
-   vector<map<PFParticle*,TMatrixDSym>> gamCovMatrix; // Maximum 4 x (3x3)
+  RooRealVar* pT_fsr[2][2];
+  RooRealVar* eta_fsr[2][2];
+  RooRealVar* phi_fsr[2][2];
+  RooRealVar* pTbar_fsr[2][2];
+  RooRealVar* etabar_fsr[2][2];
+  RooRealVar* phibar_fsr[2][2];
 
-   TMatrixDSym fitCovMatrix;
-   RooGaussian* momCovConstraints;
+  RooFormulaVar* px_ferm[2][2];
+  RooFormulaVar* py_ferm[2][2];
+  RooFormulaVar* pz_ferm[2][2];
+  RooFormulaVar* E_ferm[2][2];
+  RooFormulaVar* px_fsr[2][2];
+  RooFormulaVar* py_fsr[2][2];
+  RooFormulaVar* pz_fsr[2][2];
+  RooFormulaVar* E_fsr[2][2];
+
+  RooFormulaVar* pTdiff_ferm[2][2];
+  RooFormulaVar* etadiff_ferm[2][2];
+  RooFormulaVar* phidiff_ferm[2][2];
+  RooFormulaVar* pTdiff_fsr[2][2];
+  RooFormulaVar* etadiff_fsr[2][2];
+  RooFormulaVar* phidiff_fsr[2][2];
+
+  RooFormulaVar* m1;
+  RooFormulaVar* m2;
+  RooFormulaVar* m12;
+  RooRealVar* h1;
+  RooRealVar* h2;
+  RooRealVar* hs;
+  RooRealVar* Phi;
+  RooRealVar* Phi1;
+  RooRealVar* Y;
+
+  RooSpin::VdecayType Vdecay1;
+  RooSpin::VdecayType Vdecay2;
+  RooSpin::modelMeasurables measurables;
+  const Double_t sqrts;
+  const Int_t intCodeStart;
+
+  std::vector<map<reco::Candidate*, TMatrixDSym>> fermCovMatrix; // Maximum 4 x (3x3)
+  std::vector<map<pat::PFParticle*, TMatrixDSym>> gamCovMatrix; // Maximum 4 x (3x3)
+
+  TMatrixDSym fitCovMatrix;
+  RooGaussian* momCovConstraints;
+  RooFitResult* fitRes;
+
+
+  virtual void constructVariables();
+  virtual void destroyVariables();
+
+  virtual void constructPdfFactory();
+  virtual void destroyPdfFactory();
 };
 
 #endif
