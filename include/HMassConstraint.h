ed49e5fe920edd9308b6e05466f8a9fdbfb11ccd
usarica <ulascan90@gmail.com>
2016-04-19 08:39:05 +0200

    Add more variables

--- include/HMassConstraint.h
+++ include/HMassConstraint.h
@@ -17,7 +17,7 @@
 #include "RooDataSet.h"
 #include "RooMinuit.h"
 #include "RooFormulaVar.h"
-#include "RooAddPdf.h"
+#include "RooProdPdf.h"
 #include "RooGenericPdf.h"
 #include "RooFitResult.h"
 #include <HMassConstraint/interface/ScalarPdfFactory_ggH.h>
@@ -30,7 +30,15 @@ class HMassConstraint {
 public:
 
   // Member functions
-  HMassConstraint(Double_t sqrts_, RooSpin::VdecayType Vdecay1_, RooSpin::VdecayType Vdecay2_); // See RooSpin.h for what enumerators are available.
+  HMassConstraint(
+    const Double_t sqrts_, // 7, 8, 13, 14 etc. in units of TeV
+    // See RooSpin.h for what enumerators are available:
+    RooSpin::VdecayType Vdecay1_,
+    RooSpin::VdecayType Vdecay2_,
+    const Int_t X_spin_=0, // 0=spin-0, 2=spin-2
+    // Do not touch the arguments below at the moment
+    const Int_t intCodeStart_=RooSpin::prime_h1*RooSpin::prime_h2*RooSpin::prime_Phi*RooSpin::prime_hs*RooSpin::prime_Phi1
+    );
   virtual ~HMassConstraint();
 
   void reset(); // { if(fit_res!=0) delete fit_res; }
@@ -40,24 +48,31 @@ public:
   RooFitResult* fit(); // Does the fit and returns a pointer to the fitresult.
 
   // Data members
-  RooSpin* pdf;
+  RooSpin* spinPDF;
   SpinPdfFactory* pdfFactory;
 
 private:
 
-  RooRealVar* mass_ferm[2][2];
+  // Data members
+  const Double_t sqrts;
+  RooSpin::VdecayType Vdecay1;
+  RooSpin::VdecayType Vdecay2;
+  const Int_t X_spin;
+  const Int_t intCodeStart;
+
   RooRealVar* pT_ferm[2][2];
-  RooRealVar* eta_ferm[2][2];
+  RooRealVar* lambda_ferm[2][2]; // lambda = Pi/2 - theta
   RooRealVar* phi_ferm[2][2];
+  RooRealVar* massbar_ferm[2][2];
   RooRealVar* pTbar_ferm[2][2];
-  RooRealVar* etabar_ferm[2][2];
+  RooRealVar* lambdabar_ferm[2][2];
   RooRealVar* phibar_ferm[2][2];
 
   RooRealVar* pT_fsr[2][2];
-  RooRealVar* eta_fsr[2][2];
+  RooRealVar* lambda_fsr[2][2];
   RooRealVar* phi_fsr[2][2];
   RooRealVar* pTbar_fsr[2][2];
-  RooRealVar* etabar_fsr[2][2];
+  RooRealVar* lambdabar_fsr[2][2];
   RooRealVar* phibar_fsr[2][2];
 
   RooFormulaVar* px_ferm[2][2];
@@ -68,29 +83,32 @@ private:
   RooFormulaVar* py_fsr[2][2];
   RooFormulaVar* pz_fsr[2][2];
   RooFormulaVar* E_fsr[2][2];
+  RooFormulaVar* px_Hdaughter[2][2];
+  RooFormulaVar* py_Hdaughter[2][2];
+  RooFormulaVar* pz_Hdaughter[2][2];
+  RooFormulaVar* E_Hdaughter[2][2];
+  RooFormulaVar* m_Hdaughter[2][2]; // Use this for the extra phasespace multipliers
 
   RooFormulaVar* pTdiff_ferm[2][2];
-  RooFormulaVar* etadiff_ferm[2][2];
+  RooFormulaVar* lambdadiff_ferm[2][2];
   RooFormulaVar* phidiff_ferm[2][2];
   RooFormulaVar* pTdiff_fsr[2][2];
-  RooFormulaVar* etadiff_fsr[2][2];
+  RooFormulaVar* lambdadiff_fsr[2][2];
   RooFormulaVar* phidiff_fsr[2][2];
 
-  RooFormulaVar* m1;
-  RooFormulaVar* m2;
-  RooFormulaVar* m12;
+  RooFormulaVar* beta_Vdaughter[2];
+
+  RooFormulaVar* m[3];
   RooRealVar* h1;
   RooRealVar* h2;
   RooRealVar* hs;
   RooRealVar* Phi;
   RooRealVar* Phi1;
   RooRealVar* Y;
-
-  RooSpin::VdecayType Vdecay1;
-  RooSpin::VdecayType Vdecay2;
   RooSpin::modelMeasurables measurables;
-  const Double_t sqrts;
-  const Int_t intCodeStart;
+
+  ScalarPdfFactory_ggH* hvvFactory;
+  TensorPdfFactory_XVV* xvvFactory;
 
   std::vector<map<reco::Candidate*, TMatrixDSym>> fermCovMatrix; // Maximum 4 x (3x3)
   std::vector<map<pat::PFParticle*, TMatrixDSym>> gamCovMatrix; // Maximum 4 x (3x3)
@@ -100,11 +118,13 @@ private:
   RooFitResult* fitRes;
 
 
+  // Member functions
   virtual void constructVariables();
   virtual void destroyVariables();
 
   virtual void constructPdfFactory();
   virtual void destroyPdfFactory();
+
 };
 
 #endif
--- src/HMassConstraint.cpp
+++ src/HMassConstraint.cpp
@@ -7,94 +7,108 @@ using namespace pat;
 
 
 HMassConstraint::HMassConstraint(
-  sqrts(sqrts_),
+  const Double_t sqrts_,
   RooSpin::VdecayType Vdecay1_,
-  RooSpin::VdecayType Vdecay2_
+  RooSpin::VdecayType Vdecay2_,
+  const Int_t X_spin_,
+  const Int_t intCodeStart_
   ) :
+  sqrts(sqrts_*1000.),
   Vdecay1(Vdecay1_),
   Vdecay2(Vdecay2_),
-  intCodeStart(RooSpin::prime_h1*RooSpin::prime_h2*RooSpin::prime_hs*RooSpin::prime_Phi*RooSpin::prime_Phi1)
+  X_spin(X_spin_),
+  intCodeStart(intCodeStart_)
 {
+  if (X_spin!=0 && X_spin!=2){
+    cerr << "HMassConstraint::HMassConstraint: Only spin-0 or -2 are supported!" << endl;
+    assert(0);
+  }
+
   constructVariables();
+  constructPdfFactory();
 }
 
+~HMassConstraint::HMassConstraint(){
+  destroyPdfFactory();
+  destroyVariables();
+}
+
+
 void HMassConstraint::constructVariables(){
+  RooArgList m12_args;
   for (int iZ=0; iZ<2; iZ++){
+    RooArgList mHdaughter_args;
     for (int iferm=0; iferm<2; iferm++){
-      mass_ferm[iZ][iferm] = new RooRealVar(Form("mass_Z%iFermion%i", iZ+1, iferm+1), "", 0., -sqrts, sqrts);
       pT_ferm[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%i", iZ+1, iferm+1), "", 0., 0., sqrts);
-      eta_ferm[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%i", iZ+1, iferm+1), "", 0., 0., sqrts);
+      lambda_ferm[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%i", iZ+1, iferm+1), "", 0., 0., sqrts);
       phi_ferm[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%i", iZ+1, iferm+1), "", 0., 0., sqrts);
+      massbar_ferm[iZ][iferm] = new RooRealVar(Form("mass_Z%iFermion%i", iZ+1, iferm+1), "", 0., -sqrts, sqrts);
       pTbar_ferm[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%i", iZ+1, iferm+1), "", 0., 0., sqrts);
-      etabar_ferm[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%i", iZ+1, iferm+1), "", 0., 0., sqrts);
+      lambdabar_ferm[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%i", iZ+1, iferm+1), "", 0., 0., sqrts);
       phibar_ferm[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%i", iZ+1, iferm+1), "", 0., 0., sqrts);
 
       pT_fsr[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "", 0., 0., sqrts);
-      eta_fsr[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "", 0., 0., sqrts);
+      lambda_fsr[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "", 0., 0., sqrts);
       phi_fsr[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "", 0., 0., sqrts);
       pTbar_fsr[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "", 0., 0., sqrts);
-      etabar_fsr[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "", 0., 0., sqrts);
+      lambdabar_fsr[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "", 0., 0., sqrts);
       phibar_fsr[iZ][iferm] = new RooRealVar(Form("pTRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "", 0., 0., sqrts);
 
-      mass_ferm[iZ][iferm]->removeMin();
-      mass_ferm[iZ][iferm]->removeMax();
+      massbar_ferm[iZ][iferm]->removeMin();
+      massbar_ferm[iZ][iferm]->removeMax();
 
       pT_ferm[iZ][iferm]->removeMax();
-      eta_ferm[iZ][iferm]->removeMax();
+      lambda_ferm[iZ][iferm]->removeMax();
       phi_ferm[iZ][iferm]->removeMax();
       pTbar_ferm[iZ][iferm]->removeMax();
-      etabar_ferm[iZ][iferm]->removeMax();
+      lambdabar_ferm[iZ][iferm]->removeMax();
       phibar_ferm[iZ][iferm]->removeMax();
       pT_fsr[iZ][iferm]->removeMax();
-      eta_fsr[iZ][iferm]->removeMax();
+      lambda_fsr[iZ][iferm]->removeMax();
       phi_fsr[iZ][iferm]->removeMax();
       pTbar_fsr[iZ][iferm]->removeMax();
-      etabar_fsr[iZ][iferm]->removeMax();
+      lambdabar_fsr[iZ][iferm]->removeMax();
       phibar_fsr[iZ][iferm]->removeMax();
 
-      E_ferm[iZ][iferm] = new RooFormulaVar(Form("ERefit_Z%iFermion%i", iZ+1, iferm+1), "sqrt( pow(@0,2)+pow(@1*cosh(@2),2) )", RooArgList(*(mass_ferm[iZ][iferm]), *(pT_ferm[iZ][iferm]), *(eta_ferm[iZ][iferm])));
+      E_ferm[iZ][iferm] = new RooFormulaVar(Form("ERefit_Z%iFermion%i", iZ+1, iferm+1), "sqrt( pow(@0,2)+pow(@1/cos(@2),2) )", RooArgList(*(massbar_ferm[iZ][iferm]), *(pT_ferm[iZ][iferm]), *(lambda_ferm[iZ][iferm])));
       px_ferm[iZ][iferm] = new RooFormulaVar(Form("pxRefit_Z%iFermion%i", iZ+1, iferm+1), "(@0*cos(@1))", RooArgList(*(pT_ferm[iZ][iferm]), *(phi_ferm[iZ][iferm])));
       py_ferm[iZ][iferm] = new RooFormulaVar(Form("pyRefit_Z%iFermion%i", iZ+1, iferm+1), "(@0*sin(@1))", RooArgList(*(pT_ferm[iZ][iferm]), *(phi_ferm[iZ][iferm])));
-      pz_ferm[iZ][iferm] = new RooFormulaVar(Form("pzRefit_Z%iFermion%i", iZ+1, iferm+1), "(@0*sinh(@1))", RooArgList(*(pT_ferm[iZ][iferm]), *(eta_ferm[iZ][iferm])));
-      E_fsr[iZ][iferm] = new RooFormulaVar(Form("ERefit_Z%iFermion%iFSR", iZ+1, iferm+1), "sqrt( pow(@0*cosh(@1),2) )", RooArgList(*(pT_fsr[iZ][iferm]), *(eta_fsr[iZ][iferm])));
+      pz_ferm[iZ][iferm] = new RooFormulaVar(Form("pzRefit_Z%iFermion%i", iZ+1, iferm+1), "(@0*tan(@1))", RooArgList(*(pT_ferm[iZ][iferm]), *(lambda_ferm[iZ][iferm])));
+      E_fsr[iZ][iferm] = new RooFormulaVar(Form("ERefit_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0/cos(@1))", RooArgList(*(pT_fsr[iZ][iferm]), *(lambda_fsr[iZ][iferm])));
       px_fsr[iZ][iferm] = new RooFormulaVar(Form("pxRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0*cos(@1))", RooArgList(*(pT_fsr[iZ][iferm]), *(phi_fsr[iZ][iferm])));
       py_fsr[iZ][iferm] = new RooFormulaVar(Form("pyRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0*sin(@1))", RooArgList(*(pT_fsr[iZ][iferm]), *(phi_fsr[iZ][iferm])));
-      pz_fsr[iZ][iferm] = new RooFormulaVar(Form("pzRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0*sinh(@1))", RooArgList(*(pT_fsr[iZ][iferm]), *(eta_fsr[iZ][iferm])));
+      pz_fsr[iZ][iferm] = new RooFormulaVar(Form("pzRefit_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0*tan(@1))", RooArgList(*(pT_fsr[iZ][iferm]), *(lambda_fsr[iZ][iferm])));
+      E_Hdaughter[iZ][iferm] = new RooFormulaVar(Form("ERefit_Z%iFermion%i", iZ+1, iferm+1), "(@0+@1)", RooArgList(*(E_ferm[iZ][iferm]), *(E_fsr[iZ][iferm])));
+      px_Hdaughter[iZ][iferm] = new RooFormulaVar(Form("pxRefit_Z%iFermion%i", iZ+1, iferm+1), "(@0+@1)", RooArgList(*(px_ferm[iZ][iferm]), *(px_fsr[iZ][iferm])));
+      py_Hdaughter[iZ][iferm] = new RooFormulaVar(Form("pyRefit_Z%iFermion%i", iZ+1, iferm+1), "(@0+@1)", RooArgList(*(py_ferm[iZ][iferm]), *(py_fsr[iZ][iferm])));
+      pz_Hdaughter[iZ][iferm] = new RooFormulaVar(Form("pzRefit_Z%iFermion%i", iZ+1, iferm+1), "(@0+@1)", RooArgList(*(pz_ferm[iZ][iferm]), *(pz_fsr[iZ][iferm])));
+      m_Hdaughter[iZ][iferm] = new RooFormulaVar(Form("m_Z%iFermion%iRefit", iZ+1, iferm+1), "sqrt( pow(@0,2)-pow(@1,2)-pow(@2,2)-pow(@3,2) )", RooArgList(*(E_Hdaughter[iZ][iferm]), *(px_Hdaughter[iZ][iferm]), *(py_Hdaughter[iZ][iferm]), *(pz_Hdaughter[iZ][iferm])));
+
+      mHdaughter_args.add(*(E_Hdaughter[iZ][iferm]));
+      mHdaughter_args.add(*(px_Hdaughter[iZ][iferm]));
+      mHdaughter_args.add(*(py_Hdaughter[iZ][iferm]));
+      mHdaughter_args.add(*(pz_Hdaughter[iZ][iferm]));
+      m12_args.add(mHdaughter_args);
 
       pTdiff_ferm[iZ][iferm] = new RooFormulaVar(Form("pTDiff_Z%iFermion%i", iZ+1, iferm+1), "(@0-@1)", RooArgList(*(pT_ferm[iZ][iferm]), *(pTbar_ferm[iZ][iferm])));
-      etadiff_ferm[iZ][iferm] = new RooFormulaVar(Form("etaDiff_Z%iFermion%i", iZ+1, iferm+1), "(@0-@1)", RooArgList(*(eta_ferm[iZ][iferm]), *(etabar_ferm[iZ][iferm])));
+      lambdadiff_ferm[iZ][iferm] = new RooFormulaVar(Form("lambdaDiff_Z%iFermion%i", iZ+1, iferm+1), "(@0-@1)", RooArgList(*(lambda_ferm[iZ][iferm]), *(lambdabar_ferm[iZ][iferm])));
       phidiff_ferm[iZ][iferm] = new RooFormulaVar(Form("phiDiff_Z%iFermion%i", iZ+1, iferm+1), "(@0-@1)", RooArgList(*(phi_ferm[iZ][iferm]), *(phibar_ferm[iZ][iferm])));
       pTdiff_fsr[iZ][iferm] = new RooFormulaVar(Form("pTDiff_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0-@1)", RooArgList(*(pT_fsr[iZ][iferm]), *(pTbar_fsr[iZ][iferm])));
-      etadiff_fsr[iZ][iferm] = new RooFormulaVar(Form("etaDiff_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0-@1)", RooArgList(*(eta_fsr[iZ][iferm]), *(etabar_fsr[iZ][iferm])));
+      lambdadiff_fsr[iZ][iferm] = new RooFormulaVar(Form("lambdaDiff_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0-@1)", RooArgList(*(lambda_fsr[iZ][iferm]), *(lambdabar_fsr[iZ][iferm])));
       phidiff_fsr[iZ][iferm] = new RooFormulaVar(Form("phiDiff_Z%iFermion%iFSR", iZ+1, iferm+1), "(@0-@1)", RooArgList(*(phi_fsr[iZ][iferm]), *(phibar_fsr[iZ][iferm])));
     }
+
+    // Construct m1/m2
+    m[iZ] = new RooFormulaVar(Form("m%iRefit", iZ+1), "sqrt( pow(@0+@4,2)-pow(@1+@5,2)-pow(@2+@6,2)-pow(@3+@7,2) )", mHdaughter_args);
+
+    // This beta should multiply the spinPDF bc. having massive fermions have additional scale factors. These factors are even more relevant when FSR is present!
+    beta_Vdaughter[iZ] = new RooFormulaVar(Form("betaV%iRefit", iZ+1), "sqrt( max(1.0e-15, ( 1.-pow((@1+@2)/@0,2) )*( 1.-pow((@1-@2)/@0,2) ) ) )", RooArgList(*(m[iZ]), *(m_Hdaughter[iZ][0]), *(m_Hdaughter[iZ][1])));
   }
-
-  // Construct m1
-  RooArgList m1_args;
-  for (int iZ=0; iZ<1; iZ++){ for (int iferm=0; iferm<2; iferm++){ m1_args.add(*(E_ferm[iZ][iferm])); } }; for (int iZ=0; iZ<1; iZ++){ for (int iferm=0; iferm<2; iferm++){ m1_args.add(*(E_fsr[iZ][iferm])); } };
-  for (int iZ=0; iZ<1; iZ++){ for (int iferm=0; iferm<2; iferm++){ m1_args.add(*(px_ferm[iZ][iferm])); } }; for (int iZ=0; iZ<1; iZ++){ for (int iferm=0; iferm<2; iferm++){ m1_args.add(*(px_fsr[iZ][iferm])); } };
-  for (int iZ=0; iZ<1; iZ++){ for (int iferm=0; iferm<2; iferm++){ m1_args.add(*(py_ferm[iZ][iferm])); } }; for (int iZ=0; iZ<1; iZ++){ for (int iferm=0; iferm<2; iferm++){ m1_args.add(*(py_fsr[iZ][iferm])); } };
-  for (int iZ=0; iZ<1; iZ++){ for (int iferm=0; iferm<2; iferm++){ m1_args.add(*(pz_ferm[iZ][iferm])); } }; for (int iZ=0; iZ<1; iZ++){ for (int iferm=0; iferm<2; iferm++){ m1_args.add(*(pz_fsr[iZ][iferm])); } };
-  m1 = new RooFormulaVar("m1Refit", "sqrt( pow(@0+@1+@2+@3,2)-pow(@4+@5+@6+@7,2)-pow(@8+@9+@10+@11,2)-pow(@12+@13+@14+@15,2) )", m1_args);
-
-  // Construct m2
-  RooArgList m2_args;
-  for (int iZ=1; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m2_args.add(*(E_ferm[iZ][iferm])); } }; for (int iZ=1; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m2_args.add(*(E_fsr[iZ][iferm])); } };
-  for (int iZ=1; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m2_args.add(*(px_ferm[iZ][iferm])); } }; for (int iZ=1; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m2_args.add(*(px_fsr[iZ][iferm])); } };
-  for (int iZ=1; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m2_args.add(*(py_ferm[iZ][iferm])); } }; for (int iZ=1; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m2_args.add(*(py_fsr[iZ][iferm])); } };
-  for (int iZ=1; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m2_args.add(*(pz_ferm[iZ][iferm])); } }; for (int iZ=1; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m2_args.add(*(pz_fsr[iZ][iferm])); } };
-  m2 = new RooFormulaVar("m2Refit", "sqrt( pow(@0+@1+@2+@3,2)-pow(@4+@5+@6+@7,2)-pow(@8+@9+@10+@11,2)-pow(@12+@13+@14+@15,2) )", m2_args);
-
   // Construct m12
-  RooArgList m12_args;
-  for (int iZ=0; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m12_args.add(*(E_ferm[iZ][iferm])); } }; for (int iZ=0; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m12_args.add(*(E_fsr[iZ][iferm])); } };
-  for (int iZ=0; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m12_args.add(*(px_ferm[iZ][iferm])); } }; for (int iZ=0; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m12_args.add(*(px_fsr[iZ][iferm])); } };
-  for (int iZ=0; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m12_args.add(*(py_ferm[iZ][iferm])); } }; for (int iZ=0; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m12_args.add(*(py_fsr[iZ][iferm])); } };
-  for (int iZ=0; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m12_args.add(*(pz_ferm[iZ][iferm])); } }; for (int iZ=0; iZ<2; iZ++){ for (int iferm=0; iferm<2; iferm++){ m12_args.add(*(pz_fsr[iZ][iferm])); } };
-  m12 = new RooFormulaVar("m12Refit", "sqrt( pow(@0+@1+@2+@3+@4+@5+@6+@7,2)-pow(@8+@9+@10+@11+@12+@13+@14+@15,2)-pow(@16+@17+@18+@19+@20+@21+@22+@23,2)-pow(@24+@25+@26+@27+@28+@29+@30+@31,2) )", m12_args);
+  m12 = new RooFormulaVar("m12Refit", "sqrt( pow(@0+@4+@8+@12,2)-pow(@1+@5+@9+@13,2)-pow(@2+@6+@10+@14,2)-pow(@3+@7+@11+@15,2) )", m12_args);
 
   // Variables integrated over
+  // Should be re-written in terms of pT, lambda and phi at some point...
   hs = new RooRealVar("Gencosthetastar", "cos#theta^{*}", -1, 1);
   h1 = new RooRealVar("GenhelcosthetaZ1", "cos#theta_{1}", -1, 1);
   h2 = new RooRealVar("GenhelcosthetaZ2", "cos#theta_{2}", -1, 1);
@@ -102,74 +116,99 @@ void HMassConstraint::constructVariables(){
   Phi1 = new RooRealVar("GenphistarZ1", "#Phi_{1}", -TMath::Pi(), TMath::Pi());
   Y = new RooRealVar("GenY", "Y", 0);
 
-  // Initialize the meaurables
-  measurables.m1 = m1;
-  measurables.m2 = m2;
-  measurables.m12 = m12;
-  measurables.h1 = h1;
-  measurables.h2 = h2;
-  measurables.Phi = Phi;
-  measurables.hs = hs;
-  measurables.Phi1 = Phi1;
+  // Initialize the meaurables: Set those always integrated over to 0
+  measurables.m1 = m[0];
+  measurables.m2 = m[1];
+  measurables.m12 = m[2];
+  if (intCodeStart%RooSpin::prime_h1 != 0) measurables.h1 = h1;
+  else measurables.h1 = 0;
+  if (intCodeStart%RooSpin::prime_h2 != 0) measurables.h2 = h2;
+  else measurables.h2 = 0;
+  if (intCodeStart%RooSpin::prime_Phi != 0) measurables.Phi = Phi;
+  else measurables.Phi = 0;
+  if (intCodeStart%RooSpin::prime_hs != 0) measurables.hs = hs;
+  else measurables.hs = 0;
+  if (intCodeStart%RooSpin::prime_Phi1 != 0) measurables.Phi1 = Phi1;
+  else measurables.Phi1 = 0;
   measurables.Y = Y;
 }
 void HMassConstraint::constructPdfFactory(){
-  pdfFactory = new ScalarPdfFactory_ggH(measurables, false, Vdecay1, Vdecay2);
-  ((ScalarPdfFactory_ggH*)pdfFactory)->makeParamsConst(false);
+  hvvFactory=0;
+  xvvFactory=0;
+  if (X_spin==0){
+    hvvFactory = new ScalarPdfFactory_ggH(measurables, false, Vdecay1, Vdecay2, true); // First false for acceptance, then true for always-on-shell H
+    hvvFactory->makeParamsConst(false); // So that we can play with couplings
+    spinPDF = hvvFactory->getPDF();
+    pdfFactory = hvvFactory;
+  }
+  else if (X_spin==2){
+    xvvFactory = new TensorPdfFactory_XVV(measurables, Vdecay1, Vdecay2, true); // true for always-on-shell X
+    xvvFactory->makeParamsConst(false); // So that we can play with couplings
+    spinPDF = xvvFactory->getPDF();
+    pdfFactory = xvvFactory;
+  }
 }
 
 void HMassConstraint::destroyVariables(){
-  // Destroy in reverse order of creation
+  // Destroy in ~reverse order of creation
   delete h1;
   delete h2;
   delete hs;
   delete Phi;
   delete Phi1;
   delete Y;
-  delete m1;
-  delete m2;
-  delete m12;
-  for (int iZ=0; iZ<2; iZ++){
-    for (int iferm=0; iferm<2; iferm++){
+
+  delete m[2];
+  for (int iZ=1; iZ>=0; iZ--){
+    delete beta_Vdaughter[iZ];
+    delete m[iZ];
+
+    for (int iferm=1; iferm>=0; iferm--){
+
       delete pTdiff_ferm[iZ][iferm];
-      delete etadiff_ferm[iZ][iferm];
+      delete lambdadiff_ferm[iZ][iferm];
       delete phidiff_ferm[iZ][iferm];
       delete pTdiff_fsr[iZ][iferm];
-      delete etadiff_fsr[iZ][iferm];
+      delete lambdadiff_fsr[iZ][iferm];
       delete phidiff_fsr[iZ][iferm];
 
-      delete px_ferm[iZ][iferm];
-      delete py_ferm[iZ][iferm];
-      delete pz_ferm[iZ][iferm];
-      delete E_ferm[iZ][iferm];
+      delete E_Hdaughter[iZ][iHdaughter];
+      delete px_Hdaughter[iZ][iHdaughter];
+      delete py_Hdaughter[iZ][iHdaughter];
+      delete pz_Hdaughter[iZ][iHdaughter];
+      delete m_Hdaughter[iZ][iferm];
+
+      delete E_fsr[iZ][iferm];
       delete px_fsr[iZ][iferm];
       delete py_fsr[iZ][iferm];
       delete pz_fsr[iZ][iferm];
-      delete E_fsr[iZ][iferm];
-
-      delete pT_ferm[iZ][iferm];
-      delete eta_ferm[iZ][iferm];
-      delete phi_ferm[iZ][iferm];
-      delete pTbar_ferm[iZ][iferm];
-      delete etabar_ferm[iZ][iferm];
-      delete phibar_ferm[iZ][iferm];
+      delete E_ferm[iZ][iferm];
+      delete px_ferm[iZ][iferm];
+      delete py_ferm[iZ][iferm];
+      delete pz_ferm[iZ][iferm];
 
       delete pT_fsr[iZ][iferm];
-      delete eta_fsr[iZ][iferm];
+      delete lambda_fsr[iZ][iferm];
       delete phi_fsr[iZ][iferm];
       delete pTbar_fsr[iZ][iferm];
-      delete etabar_fsr[iZ][iferm];
+      delete lambdabar_fsr[iZ][iferm];
       delete phibar_fsr[iZ][iferm];
+
+      delete pT_ferm[iZ][iferm];
+      delete lambda_ferm[iZ][iferm];
+      delete phi_ferm[iZ][iferm];
+      delete massbar_ferm[iZ][iferm];
+      delete pTbar_ferm[iZ][iferm];
+      delete lambdabar_ferm[iZ][iferm];
+      delete phibar_ferm[iZ][iferm];
     }
   }
 }
 void HMassConstraint::destroyPdfFactory(){
-  delete pdfFactory;
-}
-
-virtual ~HMassConstraint::HMassConstraint(){
-  destroyPdfFactory();
-  destroyVariables();
+  // Only one of these is true; no need to delete pdfFactory since it is simply a mother-pointer to either of these.
+  pdfFactory=0;
+  if (hvvFactory!=0){ delete hvvFactory; hvvFactory=0; }
+  if (xvvFactory!=0){ delete xvvFactory; xvvFactory=0; }
 }
 
 
